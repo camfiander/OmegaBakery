ef38061ed01bdb36ada5403af514176bb61a4c58
athul <athul041@gmail.com>
2022-03-12 17:18:59 -0400

    Abstract Factory design for products.

---
+++ OmegaBakery/Domain/Products/BakeryFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaBakery.Domain.Products
+{
+    public class BakeryFactory : IProductFactory
+    {
+        public IInHouseProduct createInHouseProduct() => new BakeryInHouseProduct();
+
+        public IOutsideProduct createOutsideProduct() => new BakeryOutsideProduct();
+    }
+}
---
+++ OmegaBakery/Domain/Products/IProductFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaBakery.Domain.Products
+{
+    internal interface IProductFactory
+    {
+        public IInHouseProduct createInHouseProduct();
+        public IOutsideProduct createOutsideProduct();
+    }
+}
