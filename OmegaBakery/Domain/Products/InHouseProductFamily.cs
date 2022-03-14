fc0dabad60ebb802d3b041474eef93d2791f287c
camfiander <cfiande1@unb.ca>
2022-03-13 22:13:34 -0300

    Add ProductFamily classes

---
+++ OmegaBakery/Domain/Products/InHouseProductFamily.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaBakery.Domain.Products
+{
+    internal class InHouseProductFamily : ProductFamily
+    {
+        private List<BakeryInHouseProduct> _products;
+        private string _name;
+
+        public override List<IProduct> Products => _products.Cast<IProduct>().ToList();
+
+        public override string Name => _name;
+
+        public InHouseProductFamily(string name, List<BakeryInHouseProduct> products)
+        {
+            _name = name;
+            _products = products;
+        }
+
+    }
+}
---
+++ OmegaBakery/Domain/Products/ProductFamily.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaBakery.Domain.Products
+{
+    internal abstract class ProductFamily
+    {
+        public abstract List<IProduct> Products { get; }
+
+        public abstract string Name { get; }
+
+        public string Render()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach(IProduct product in Products)
+            {
+                sb.AppendLine(product.Render());
+            }
+
+            return sb.ToString();
+        }
+
+        
+    }
+}
