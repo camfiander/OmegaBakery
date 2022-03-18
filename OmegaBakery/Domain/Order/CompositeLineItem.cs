7fde0f821d38df165c357dbf94b821df8701d7ec
camfiander <cfiande1@unb.ca>
2022-03-18 13:21:44 -0300

    Add Compsite Line item

---
+++ OmegaBakery/Domain/Order/CompositeLineItem.cs
@@ -0,0 +1,28 @@
+﻿using OmegaBakery.Domain.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaBakery.Domain.Order
+{
+    internal class CompositeLineItem : ILineItem
+    {
+        private List<ILineItem> _items;
+        public int Count => _items.Sum(x => x.Count);
+
+        public double Subtotal => _items.Sum(x => x.Subtotal);
+
+        public string Render()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in _items)
+            {
+                sb.Append("\t");
+                sb.Append(item.Render());
+            }
+            return sb.ToString();
+        }
+    }
+}
